07dd97de2424fb072b92ebad954c67d3e35ce882
Thibaut Stachnick <thibautstachnick@gmail.com>
2023-09-09 21:37:21 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/accueil.html.twig",
+            "./assets/styles/app.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
